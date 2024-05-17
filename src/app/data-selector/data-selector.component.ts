2f8617c48edf2be94d5267c1c9163755edcbcae4
Boss <bossnoname2018@gmail.com>
2024-05-17 10:54:21 +0700

    add data-component

---
+++ src/app/data-selector/data-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-selector.component.html',
+  styleUrl: './data-selector.component.css'
+})
+export class DataSelectorComponent {
+
+}
--- src/app/dog.service.ts
+++ src/app/dog.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
 
-  constructor() { }
+  private apiUrl = 'https://dog.ceo/api/breeds/image/random';
+
+  constructor(private http: HttpClient) { }
+
+  getRandomDogImage(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
